29bf074c9847e34422a95133660b80f45b8defef
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-27 15:38:35 +0300

    merge two dictionaries

--- mergeDictionaries.py
+++ mergeDictionaries.py
@@ -1,1 +1,12 @@
 # merge two dictionaries 
+
+my_dict1 = {"frank": 22,
+            "Ancilla": 34,}
+my_dict2 = {"city": "Nairobi",
+            "country": "Kenya"}
+
+major_Dict = {}
+
+major_Dict.update(my_dict1) # can also use the | in merging (major_Dict = my_dict1 | my_dict2)
+major_Dict.update(my_dict2)
+print(major_Dict)
