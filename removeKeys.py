38f59f7f7fa62658512e1d6844bb1556c07d169a
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-28 09:28:35 +0300

    remove key-value pairs where the value is less than 10

--- removeKeys.py
+++ removeKeys.py
@@ -1,1 +1,18 @@
 # remove key-value pairs where the value is less than 10
+
+students = { }
+num = int(input("Enter the size of your dictionary:"))
+
+for i in range(num):
+    name = input("Enter the key:")
+    score = int(input("Enter the value:"))
+    students.update({name: score})
+    
+print("your new dictionary is:", students)
+
+keys_to_remove = [key for key, value in students.items() if value < 10]
+
+for key in keys_to_remove:
+        del students[key]
+        
+print("The newest dictionary is", students)
